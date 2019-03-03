7308540001d5b1645076b6ce1e3cd0654a64e01b
Ryan Erskine <ryan.j.erskine@gmail.com>
2019-03-03 02:41:20 -0600

    Setting up Azure pipeline.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to website!');
+    expect(page.getTitleText()).toEqual('Welcome to ryanerskine.dev');
   });
 });
