b21c31869ee5dd3a59b4f653f8a549a13953a696
Ryan Erskine <ryan.j.erskine@gmail.com>
2019-03-29 01:53:15 -0500

    Fixing tests.

--- src/app/core/landing/landing.component.spec.ts
+++ src/app/core/landing/landing.component.spec.ts
@@ -1,6 +1,14 @@
+import { AuthService } from './../services/auth.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MockComponent } from 'ng-mocks';
 import { LandingComponent } from './landing.component';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCard } from '@angular/material/card';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,7 +16,20 @@ describe('LandingComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LandingComponent ]
+      imports: [
+        MatMenuModule,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      declarations: [
+        LandingComponent,
+        MockComponent(MatButton),
+        MockComponent(MatIcon),
+        MockComponent(MatToolbar),
+        MockComponent(MatToolbarRow),
+        MockComponent(MatCard)
+      ],
+      providers: []
     })
     .compileComponents();
   }));
--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    providers: []
+  })
+);
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
